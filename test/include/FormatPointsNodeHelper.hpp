444770b281ae1886e9417ee439aa2464e5631d44
nebraszka <majusxp@gmail.com>
2023-08-29 10:09:11 +0200

    Final version of format points node

---
+++ test/include/FormatPointsNodeHelper.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#include <RGLFields.hpp>
+#include <random>
+#include <scenes.hpp>
+
+constexpr int MULTIPLE_FORMATS_COUNT = 15;
+
+static std::random_device randomDevice;
+static auto randomSeed = randomDevice();
+static std::mt19937 randomGenerator{randomSeed};
+
+struct FormatPointsNodeHelper
+{
+	rgl_node_t usePointsNode = nullptr;
+	rgl_node_t compactPointsNode = nullptr;
+	rgl_node_t yieldPointsNode = nullptr;
+	std::vector<rgl_node_t> formatNodes;
+
+	FormatPointsNodeHelper() : formatNodes(MULTIPLE_FORMATS_COUNT, nullptr) {}
+
+	struct PointWithoutPadding
+	{
+		Field<XYZ_F32>::type xyz;
+		Field<IS_HIT_I32>::type isHit;
+		Field<DISTANCE_F32>::type distance;
+
+		static std::vector<rgl_field_t> getPointFields() { return {XYZ_F32, IS_HIT_I32, DISTANCE_F32}; }
+	};
+
+	static std::vector<PointWithoutPadding> generatePointsWithoutPaddings(int pointsCount)
+	{
+		std::vector<PointWithoutPadding> points;
+		points.reserve(pointsCount);
+		for (int i = 0; i < pointsCount; ++i) {
+			auto currentPoint = PointWithoutPadding{
+			    .xyz = {i, i + 1.1, i + 2.2},
+			    .isHit = i % 2,
+			    .distance = static_cast<float>(sqrt(pow(i, 2) + pow(i + 1, 2) + pow(i + 2, 2)))
+            };
+			points.push_back(currentPoint);
+		}
+		return points;
+	}
+
+	struct PointWithPadding
+	{
+		Field<XYZ_F32>::type xyz;
+		Field<PADDING_32>::type padding;
+		Field<IS_HIT_I32>::type isHit;
+		Field<DISTANCE_F32>::type distance;
+
+		static std::vector<rgl_field_t> getPointFields() { return {XYZ_F32, PADDING_32, IS_HIT_I32, DISTANCE_F32}; }
+	};
+
+	struct PointDoublePadding
+	{
+		Field<XYZ_F32>::type xyz;
+		Field<PADDING_32>::type padding;
+		Field<IS_HIT_I32>::type isHit;
+		Field<PADDING_32>::type padding2;
+		Field<DISTANCE_F32>::type distance;
+
+		static std::vector<rgl_field_t> getPointFields() { return {XYZ_F32, PADDING_32, IS_HIT_I32, PADDING_32, DISTANCE_F32}; }
+	};
+
+	struct PointTriplePadding
+	{
+		Field<XYZ_F32>::type xyz;
+		Field<PADDING_32>::type padding;
+		Field<IS_HIT_I32>::type isHit;
+		Field<PADDING_32>::type padding2;
+		Field<DISTANCE_F32>::type distance;
+		Field<PADDING_32>::type padding3;
+
+		static std::vector<rgl_field_t> getPointFields()
+		{
+			return {XYZ_F32, PADDING_32, IS_HIT_I32, PADDING_32, DISTANCE_F32, PADDING_32};
+		}
+	};
+
+	struct PointQuadrPadding
+	{
+		Field<PADDING_32>::type padding;
+		Field<XYZ_F32>::type xyz;
+		Field<PADDING_32>::type padding2;
+		Field<IS_HIT_I32>::type isHit;
+		Field<PADDING_32>::type padding3;
+		Field<DISTANCE_F32>::type distance;
+		Field<PADDING_32>::type padding4;
+
+		static std::vector<rgl_field_t> getPointFields()
+		{
+			return {PADDING_32, XYZ_F32, PADDING_32, IS_HIT_I32, PADDING_32, DISTANCE_F32, PADDING_32};
+		}
+	};
+
+	struct isHitAlonePoint
+	{
+		Field<IS_HIT_I32>::type isHit;
+
+		static std::vector<rgl_field_t> getPointFields() { return {IS_HIT_I32}; }
+	};
+
+	struct xyzAlonePoint
+	{
+		Field<XYZ_F32>::type xyz;
+
+		static std::vector<rgl_field_t> getPointFields() { return {XYZ_F32}; }
+	};
+};
